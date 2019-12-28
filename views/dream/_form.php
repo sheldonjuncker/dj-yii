143790d73952ee1a2ce30e03500f5fb782313530
Sheldon Juncker <sheldonjuncker@gmail.com>
2019-12-28 17:02:14 -0500

    Added default dream controller.

---
+++ views/dream/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\dj\Dream */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="dream-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'user_id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'dreamt_at')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'updated_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
