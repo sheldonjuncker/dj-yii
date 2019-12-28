11ffa872bed319b3141420b419b96eb510873b26
Sheldon Juncker <sheldonjuncker@gmail.com>
2019-12-28 16:19:42 -0500

    Initial commit.

---
+++ models/dj/Dream.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models\dj;
+
+use Yii;
+
+/**
+ * This is the model class for table "dj.dream".
+ *
+ * @property string $id
+ * @property int $user_id
+ * @property string $title
+ * @property string|null $description
+ * @property string $dreamt_at
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ */
+class Dream extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'dj.dream';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'user_id', 'title', 'dreamt_at'], 'required'],
+            [['user_id'], 'integer'],
+            [['description'], 'string'],
+            [['dreamt_at', 'created_at', 'updated_at'], 'safe'],
+            [['id'], 'string', 'max' => 16],
+            [['title'], 'string', 'max' => 256],
+            [['id'], 'unique'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'dreamt_at' => 'Dreamt At',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return DreamQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new DreamQuery(get_called_class());
+    }
+}
---
+++ models/dj/User.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models\dj;
+
+use Yii;
+
+/**
+ * This is the model class for table "dj.user".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'dj.user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'password'], 'required'],
+            [['email_verified_at', 'created_at', 'updated_at'], 'safe'],
+            [['name', 'email', 'password'], 'string', 'max' => 255],
+            [['remember_token'], 'string', 'max' => 100],
+            [['email'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'email_verified_at' => 'Email Verified At',
+            'password' => 'Password',
+            'remember_token' => 'Remember Token',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return UserQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new UserQuery(get_called_class());
+    }
+}
