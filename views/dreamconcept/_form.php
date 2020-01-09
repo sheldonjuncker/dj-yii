61c3631471c0104b4ab5ee332076ba250acf2654
Sheldon Juncker <sheldonjuncker@gmail.com>
2020-01-08 20:40:00 -0500

    Working out solution for selecting values via search dropdown.

--- views/dreamconcept/_form.php
+++ views/dreamconcept/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use kartik\select2\Select2;
 use yii\widgets\ActiveForm;
 
 /** @var $this yii\web\View */
@@ -14,6 +15,26 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+	<div class="form-group">
+		<?php
+		$words = \app\models\freud\Word::find()->orderBy('word DESC')->all();
+		$wordData = [];
+		foreach($words as $word)
+		{
+			$wordData[$word->id] = $word->word;
+		}
+		echo '<label class="control-label">Add Words</label>';
+		echo Select2::widget([
+			'name' => 'Concept[words]',
+			'data' => $wordData,
+			'options' => [
+				'placeholder' => 'Select words...',
+				'multiple' => true
+			],
+		]);
+		?>
+	</div>
 
     <div class="form-group">
         <?= Html::submitButton($model->getIsNewRecord() ? 'Add' : 'Edit', ['class' => 'btn btn-primary']) ?>
